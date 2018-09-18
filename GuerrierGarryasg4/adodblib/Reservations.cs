1214e47e20eab0862ba253438b7833843ff7346f
taj1217 <ajinator@ufl.edu>
2018-09-18 10:40:00 -0400

    Add files via upload

---
+++ GuerrierGarryasg4/adodblib/Reservations.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Globalization;
+namespace adodblib
+{
+    public class Reservations
+    {
+        public SqlConnection dbConnection;
+        private const string dbName = "reservations";
+
+
+
+        public Reservations(string userID, string UserPass)
+        {
+            dbConnection = new SqlConnection();
+            dbConnection.ConnectionString = String.Format("Data Source = (local); Initial Catalog =reservations; Integrated Security = True;");
+            dbConnection.Open();
+
+        }
+
+
+        public List<string> listCustomers()
+        {
+            List<string> allCustomers = new List<string>();
+            try
+            {
+                String sqlQuery = String.Format("Select * from reservations.dbo.Customer");
+                DataSet dataSet = new DataSet();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                SqlCommand cmd = new SqlCommand(sqlQuery, dbConnection);
+                dataAdapter.SelectCommand = cmd;
+                dataAdapter.Fill(dataSet);
+                DataTable dataTable = dataSet.Tables[0];
+
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    String row = "";
+                    for (int j = 0; j < dataTable.Columns.Count; j++)
+                        if (dataTable.Rows[i][j] != null)
+                        {
+                            row += dataTable.Rows[i][j].ToString() + "\t";
+                            allCustomers.Add(dataTable.Rows[i][j].ToString());
+                        }
+                        else
+                            row += "\t"; Console.WriteLine(row);
+                }
+            }
+            catch (Exception sqle)
+            {
+                Console.WriteLine(sqle.StackTrace);
+            }
+            return allCustomers;
+        }
+
+        public int TransactSQL(String[] sql, SqlConnection cnn)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlTransaction trans = cnn.BeginTransaction();
+            int n = 0;
+            cmd.Connection = cnn;
+            try
+            {
+                cmd.Transaction = trans;
+                for (int i = 0; i < sql.Length; i++)
+                {
+                    cmd.CommandText = sql[i];
+                    n += cmd.ExecuteNonQuery();
+                }
+                trans.Commit();
+            }
+            catch (InvalidOperationException ex)
+            {
+                trans.Rollback();
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                trans.Rollback();
+                throw ex;
+            }
+            return n;
+        }
+
+
+        public List<string> listReservations(int customerId)
+        {
+            List<string> allReservationsByCustomerId = new List<string>();
+            try
+            {
+                String sql = String.Format("SELECT roomno,datein,dateout,totalprice FROM reservation WHERE CustomerId  = '{0}'", customerId);
+                DataSet ds = new DataSet();
+                SqlDataAdapter da = new SqlDataAdapter();
+                SqlCommand cmd = new SqlCommand(sql, dbConnection);
+                da.SelectCommand = cmd;
+                da.Fill(ds);
+                DataTable dt = ds.Tables[0];
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    String row = "";
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                        if (dt.Rows[i][j] != null)
+                        {
+                            row += dt.Rows[i][j].ToString() + "\t";
+                            allReservationsByCustomerId.Add(dt.Rows[i][j].ToString());
+                        }
+                        else
+                            row += "\t"; Console.WriteLine(row);
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            catch (Exception ex2)
+            {
+                Console.WriteLine(ex2.StackTrace);
+            }
+
+            return allReservationsByCustomerId;
+        }
+
+        public List<string> listavailable(String datein, String dateout)
+        {
+            List<string> rv = new List<string>();
+            List<int> roomNumber = new List<int>();
+            List<string> roomType = new List<string>();
+            List<int> numberBeds = new List<int>();
+            List<double> pricing = new List<double>();
+            try
+            {
+                String sql = String.Format("select Max(Date),Min(Date) from pricing \n"
+                        + "HAVING Max(Date) >= '{0}' and Min(Date) <= '{1}'", dateout, datein);
+
+                SqlCommand s = new SqlCommand(sql, dbConnection);
+                SqlDataReader rs = s.ExecuteReader();
+                int count = 0;
+                if (rs.HasRows)
+                {
+                    while (rs.Read())
+                    {
+                        count++;
+                    }
+                }
+
+                if (count == 1)
+                {
+                    sql = String.Format("SELECT r.RoomNo, rd.RoomType, rd.numbeds, SUM(Price) "
+                            + "FROM RoomInventory r INNER JOIN RoomDetails rd INNER JOIN Pricing p ON p.RoomType= rd.RoomType "
+                            + "ON r.RoomType = rd.RoomType "
+                            + "WHERE p.Date >= '{0}' AND p.Date < '{1}' and RoomNo NOT IN "
+                            + "(SELECT RoomNo FROM Reservation "
+                            + " WHERE DateIn < '{2}' AND DateOut > '{3}' ) "
+                            + "GROUP BY r.RoomNo,rd.RoomType, rd.numbeds;", datein, dateout, dateout, datein);
+
+                    SqlCommand s2 = new SqlCommand(sql, dbConnection);
+                    rs.Close();
+                    DataSet ds = new DataSet();
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    SqlCommand cmd = new SqlCommand(sql, dbConnection);
+                    da.SelectCommand = cmd;
+                    da.Fill(ds);
+                    DataTable dt = ds.Tables[0];
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        String row = "";
+                        for (int j = 0; j < dt.Columns.Count; j++)
+                            if (dt.Rows[i][j] != null)
+                            {
+                                row += dt.Rows[i][j].ToString() + "\t";
+                            }
+                            else
+                                row += "\t"; Console.WriteLine(row); rv.Add(row);
+                    }
+                    ds.Clear();
+                }
+                rs.Close();
+
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            List<string> rooms = new List<string>();
+            foreach (var item in rv)
+            {
+                List<string> myList = item.Split('\t').ToList();
+                string x = myList[0];
+                rooms.Add(x);
+            }
+            return rooms;
+        }
+
+
+        public float Book(String cid, String roomno, String datein, String dateout)
+        {
+            List<string> sum = new List<string>();
+            float finalPrice = 0;
+            try
+            {
+                String sql = String.Format("SELECT SUM(Price) "
+                        + "FROM RoomInventory r INNER JOIN RoomDetails rd INNER JOIN Pricing p ON p.RoomType= rd.RoomType "
+                        + "ON r.RoomType = rd.RoomType WHERE p.Date >= '{0}' AND p.Date < '{1}' and RoomNo = {2} ", datein, dateout, roomno);
+
+                SqlCommand s = new SqlCommand(sql, dbConnection);
+                DataSet ds = new DataSet();
+                SqlDataAdapter da = new SqlDataAdapter();
+                SqlCommand cmd = new SqlCommand(sql, dbConnection);
+                da.SelectCommand = cmd;
+
+                da.Fill(ds);
+                DataTable dt = ds.Tables[0];
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    String row = "";
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                        if (dt.Rows[i][j] != null)
+                        {
+                            row += dt.Rows[i][j].ToString() + "\t";
+                        }
+                        else
+                            row += "\t"; Console.WriteLine(row); sum.Add(row);
+                }
+
+
+
+                String[] Arraysql = new String[1];
+                Arraysql[0] = String.Format("INSERT INTO Reservation(CustomerID, RoomNo,Datein,Dateout, TotalPrice) "
+                        + "VALUES({0},{1},'{2}','{3}',{04});", cid, roomno, datein, dateout, finalPrice);
+                TransactSQL(Arraysql, dbConnection);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            List<string> totalPrice = new List<string>();
+            foreach (var item in sum)
+            {
+                float price = float.Parse(item);
+                finalPrice += price;
+            }
+            return finalPrice;
+        }
+
+    }
+}
---
+++ GuerrierGarryasg4/asg4client/Client.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using adodblib;
+using C = System.Console;
+
+namespace asg4client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Reservations rs = new Reservations("ism6236", "ism6236bo");
+
+                List<string> listOfCustomers = rs.listCustomers();
+
+                C.WriteLine("Press L to List Reservations, B to Book, Q to quit");
+
+                string input = C.ReadLine();
+
+                while (!input.ToLower().Equals("q"))
+                {
+                    if (input.ToLower().Equals("l"))
+                    {
+                        C.WriteLine("Enter Customer ID:");
+                        string cid = C.ReadLine();
+                        int cidInt = Int32.Parse(cid);
+                        List<string> res = rs.listReservations(cidInt);
+                        foreach (string s in res)
+                        {
+                            C.WriteLine(s);
+                        }
+                    }
+                    else if (input.ToLower().Equals("b"))
+                    {
+                        C.WriteLine("Please enter a check in date - formatted as 4/20/18");
+                        string datein = C.ReadLine();
+                        C.WriteLine("Please enter a leaving date - formatted as 4/20/18");
+                        String dateout = C.ReadLine();
+                        List<string> avrooms = rs.listavailable(datein, dateout);
+                        if (avrooms.Capacity > 0)
+                        {
+                            List<string> roomnos = new List<string>();
+                            C.WriteLine(string.Format("Rooms available between {0} and {1}", datein, dateout));
+                            C.WriteLine("Press B to book or C to Cancel");
+                            input = C.ReadLine();
+
+                            if (input.ToLower().Equals("b"))
+                            {
+                                C.WriteLine("Please enter available room");
+                                input = C.ReadLine();
+
+                                while (!avrooms.Contains(input.Trim()))
+                                {
+                                    C.WriteLine("Please enter available room");
+                                    input = C.ReadLine();
+                                }
+
+                                string roomno = input;
+                                C.WriteLine("Enter Customer ID:");
+                                input = C.ReadLine();
+                                string cid = input;
+
+                                float p = rs.Book(cid, roomno, datein, dateout);
+                                if (p > 0)
+                                {
+                                    C.WriteLine(string.Format("Reservations Total Cost:{0}", p));
+                                }
+                                else
+                                {
+                                    C.WriteLine("Something is Wrong - Try Again!");
+                                }
+                            }
+                        }
+                        else
+                            C.WriteLine("No Rooms are available for the entered dates");
+                    }
+                    C.WriteLine("Press L to List Reservations, B to Book, Q to quit");
+                    input = C.ReadLine();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+
+        }
+    }
+}
+
